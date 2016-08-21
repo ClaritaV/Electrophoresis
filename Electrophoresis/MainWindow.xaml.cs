6ac5a383b4ab472fc712255c698025bffa263dfb
Andrey Bychko <courage.andrey@gmail.com>
2016-08-21 10:44:04 +0300

    Заполнена главная форма приложения.

--- Electrophoresis/MainWindow.xaml.cs
+++ Electrophoresis/MainWindow.xaml.cs
@@ -1,10 +1,49 @@
-﻿namespace Electrophoresis
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+using Microsoft.Win32;
+
+namespace Electrophoresis
 {
 	public partial class MainWindow
 	{
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private readonly OpenFileDialog openImageDialog = new OpenFileDialog
+		{
+			DefaultExt = ".jpg",
+			Filter = "JPEG-изображение|*.jpg",
+			RestoreDirectory = true,
+			Title = "Выберите изображение для анализа",
+		};
+
+		private void analysisClick(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void browseFileClick(object sender, RoutedEventArgs e)
+		{
+			if (openImageDialog.ShowDialog() == true)
+			{
+				textBoxImagePath.Text = openImageDialog.FileName;
+			}
+		}
+
+		private void showImageClick(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				imagePreview.Source = new BitmapImage(new Uri(textBoxImagePath.Text));
+			}
+			catch
+			{
+				MessageBox.Show("Невозможно загрузить изображение.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
 		}
 	}
 }
